d0a036334dc5f8ed4fd9a40ff2c40b30fc991bdf
ZOMini <zarikacr@gmail.com>
2021-12-19 15:34:12 +0300

    v 1.5

--- my_4/main/parse_2miner.py
+++ my_4/main/parse_2miner.py
@@ -2,18 +2,19 @@ import pprint
 
 from main.parse_gminer import MyDict
 
-items_del = ('24hnumreward', '24hreward', 'apiVersion', 'config', 'sumrewards', 'pageSize', 'payments', 'paymentsTotal', 'rewards', 'roundShares', 'sharesInvalid', 'stats', 'updatedAt', 'workers', 'workersOffline', 'workersTotal')
+ITEMS_DEL = ('24hnumreward', '24hreward', 'apiVersion', 'config', 'sumrewards', 'pageSize', 'payments', 'paymentsTotal', 'rewards', 'roundShares', 'sharesInvalid', 'stats', 'updatedAt', 'workers', 'workersOffline', 'workersTotal')
 
 class PoolDict(MyDict):
     def __init__(self, gm_response = {}, pool_var = {}):
         super().__init__(gm_response)
-    
+        self.pool_var = pool_var
+        
     def miner_api(self, adress):
         super().miner_api(adress)
         
     def parse_pool(self):
         pool_var = self.gm_response
-        for item in items_del:
+        for item in ITEMS_DEL:
             del pool_var[item]
         flo = float(pool_var['currentHashrate'] / 1000000)
         pool_var['currentHashrate'] = f'{flo:.2f} MH/s'
